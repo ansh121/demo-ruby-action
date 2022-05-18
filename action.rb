d9c31965ebdb320fa32c69cadc3bef7466b38c81
Anshul Choudhary <ansh121@github.com>
2022-05-18 14:10:55 +0530

    1

---
+++ action.rb
@@ -0,0 +1,10 @@
+require 'octokit'
+require 'json'
+
+octokit = Octokit::Client.new(access_token: ENV['TOKEN'])
+
+payload = JSON.parse(File.read(ENV['GITHUB_EVENT_PATH']))
+octokit.auto_paginate = true
+issues_and_prs = octokit.list_issues(repository: payload['repository']['full_name'], state: 'all')
+
+print(issues_and_prs)
---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -e
+stats = $(sh -c "ruby /action.rb $*")
+
+echo ::set-output stats=$stats
+
