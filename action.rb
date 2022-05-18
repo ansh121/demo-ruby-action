2f0f1f4e689459beca2da6e3627e8029a58cc6c8
Anshul Choudhary <ansh121@github.com>
2022-05-18 15:13:54 +0530

    2

--- action.rb
+++ action.rb
@@ -5,7 +5,7 @@ octokit = Octokit::Client.new(access_token: ENV['TOKEN'])
 
 payload = JSON.parse(File.read(ENV['GITHUB_EVENT_PATH']))
 octokit.auto_paginate = true
-issues_and_prs = octokit.issues(:repository => payload['repository']['full_name'], :state => 'all')
+issues_and_prs = octokit.issues :repository => payload['repository']['full_name'], :state => 'all'
 
 # for(item in issues_and_prs)
 
